3e5521d43cc58e474232d47a22a1a0904bf530f4
vamsi <vlanka2@asu.edu>
2020-03-14 00:31:17 -0700

    Added sqs sample functionality

--- src/main/java/UploadVideo.java
+++ src/main/java/UploadVideo.java
@@ -1,13 +1,18 @@
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.SdkClientException;
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class UploadVideo {
     public static void main(String[] args) throws IOException {
@@ -15,6 +20,8 @@ public class UploadVideo {
         String bucketName = "ccfoebucket";
         String fileObjKeyName = "screenshot";
         String fileName = "/home/vamsi/Pictures/sample.png";
+        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+        String myQueueUrl = "";
 
         try {
             //This code expects that you have AWS credentials set up per:
@@ -30,6 +37,29 @@ public class UploadVideo {
             metadata.addUserMetadata("x-amz-meta-title", "someTitle");
             request.setMetadata(metadata);
             s3Client.putObject(request);
+            System.out.println("Listing all queues in your account.\n");
+            for (final String queueUrl : sqs.listQueues().getQueueUrls()) {
+                System.out.println("  QueueUrl: " + queueUrl);
+                myQueueUrl = queueUrl;
+            }
+            System.out.println();
+            final SendMessageRequest sendMessageRequest = new SendMessageRequest(myQueueUrl, fileObjKeyName);
+            sendMessageRequest.setMessageGroupId("vamsi1");
+            final SendMessageResult sendMessageResult = sqs.sendMessage(sendMessageRequest);
+            final String sequenceNumber = sendMessageResult.getSequenceNumber();
+            final String messageId = sendMessageResult.getMessageId();
+            System.out.println("SendMessage succeed with messageId " + messageId + ", sequence number " + sequenceNumber + "\n");
+
+            System.out.println("Receiving messages from MyQueue.\n");
+            final ReceiveMessageRequest receiveMessageRequest =
+                    new ReceiveMessageRequest(myQueueUrl);
+            final List<Message> messages = sqs.receiveMessage(receiveMessageRequest)
+                    .getMessages();
+            for (final Message message : messages) {
+                System.out.println("Message");
+                System.out.println("  Body:          "
+                        + message.getBody());
+            }
         } catch (AmazonServiceException e) {
             // The call was transmitted successfully, but Amazon S3 couldn't process
             // it, so it returned an error response.
